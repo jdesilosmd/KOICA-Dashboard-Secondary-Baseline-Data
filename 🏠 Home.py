e185424bc5ffaaf2e7f4960fbcc67c1697ab9c06
Jeriel De Silos <jdesilosmd@gmail.com>
2023-10-27 09:28:12 +0800

    Add files via upload

---
+++ 🏠 Home.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+
+# Data Source
+
+projected_link = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRuOM0mbpu6ThzB3HLRcKkXk3LCwE-0n-nAptaowFoNLQekgFX7FqU3IClSjl5rlX85bi-7REtyqEJt/pub?output=xlsx"
+data_link = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRp2M0ySSYySdadzJmA93oPOYsoF_0bH_dweGzqrtYn_xS3ugEtZQnIamGXuZIxxQP6TRqcuhcPrnSO/pub?output=xlsx"
+
+
+
+### DASHBOARD CODE STARTS HERE ###
+# Title
+
+st.title('KOICA Sites in Region VIII: Secondary Data for Select Indicators in Southern Leyte')
+
+
